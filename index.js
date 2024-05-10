a0de7f1d71ccdfa5546c6c78234d6314f577174a
Morlé Koudeka <ndimorle@live.fr>
2024-05-10 19:41:37 +0000

    more generate option

--- index.js
+++ index.js
@@ -2,65 +2,75 @@
 var cryptoRandom = require("math-random");
 
 function idGenerate() {
-  var length =
-    arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 12;
-  var scope =
-    arguments.length > 1 && arguments[1] !== undefined
-      ? arguments[1]
-      : "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  if (typeof length !== "number") throw new Error("length must be a number");
-  if (typeof scope !== "string") throw new Error("scope must be a string");
-  var str = "";
-  var l = scope.length;
+    var length =
+        arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 12;
+    var scope =
+        arguments.length > 1 && arguments[1] !== undefined
+            ? arguments[1]
+            : "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (typeof length !== "number") throw new Error("length must be a number");
+    if (typeof scope !== "string") throw new Error("scope must be a string");
+    var str = "";
+    var l = scope.length;
 
-  for (var i = 0; i < length; i++) {
-    str += scope.charAt(Math.floor(Math.random() * l));
-  }
+    for (var i = 0; i < length; i++) {
+        str += scope.charAt(Math.floor(Math.random() * l));
+    }
 
-  return str;
+    return str;
 }
 
 module.exports = function customId(customId) {
-  if (typeof customId === "undefined") {
-    throw new Error(
-      "Custom Id expects an object as the argument. Make sure all values of that object are string."
+    if (typeof customId === "undefined") {
+        throw new Error(
+            "Custom Id expects an object as the argument. Make sure all values of that object are string."
+        );
+    }
+    let multiply = customId.uniqueId || 97232;
+    let crypticNotSecure = Math.floor(Math.random() * multiply);
+    let crypticSecureFun = cryptoRandom() * multiply;
+    let crypticSecure = Math.floor(crypticSecureFun);
+    let length = customId.randomLength || 2;
+    let firstId = idGenerate(
+        length,
+        crypticSecure.toString() || crypticNotSecure.toString()
     );
-  }
-  var multiply = customId.uniqueId || 97232;
-  var crypticNotSecure = Math.floor(Math.random() * multiply);
-  var crypticSecureFun = cryptoRandom() * multiply;
-  var crypticSecure = Math.floor(crypticSecureFun);
-  var length = customId.randomLength || 2;
-  var firstId = idGenerate(
-    length,
-    crypticSecure.toString() || crypticNotSecure.toString()
-  );
-  if (customId.name) {
-    var name = idGenerate(
-      length,
-      customId.name.replace(/[^a-zA-Z0-9]/g, "").toString() || ""
-    );
-  } else {
-    var name = idGenerate(length, "ABCDEFGHIJKLMNOPQRSTUVXYZ");
-  }
-  if (customId.email) {
-    var email = idGenerate(
-      length,
-      customId.email.replace(/[^a-zA-Z0-9]/g, "").toString() || ""
-    );
-  } else {
-    var email = idGenerate(length, "ABCDEFGHIJKLMNOPQRSTUVXYZ");
-  }
 
-  var lastId = idGenerate(
-    length,
-    crypticSecure.toString() || crypticNotSecure.toString()
-  );
-  var result = firstId + name + lastId + email;
+    if (customId.name) {
+        var name = idGenerate(
+            length,
+            customId.name.replace(/[^a-zA-Z0-9]/g, "").toString() || ""
+        );
+    } else {
+        var name = idGenerate(length, "ABCDEFGHIJKLMNOPQRSTUVXYZ");
+    }
 
-  if (customId.lowerCase) {
-    return result.toLowerCase();
-  } else {
-    return result.toUpperCase();
-  }
+    if (customId.email) {
+        var email = idGenerate(
+            length,
+            customId.email.replace(/[^a-zA-Z0-9]/g, "").toString() || ""
+        );
+    } else {
+        var email = idGenerate(length, "ABCDEFGHIJKLMNOPQRSTUVXYZ");
+    }
+
+    let lastId = idGenerate(
+        length,
+        crypticSecure.toString() || crypticNotSecure.toString()
+    );
+
+    let prefix = customId.prefix ? customId.prefix : '';
+    let sufix = customId.sufix ? customId.sufix : '';
+
+    if (customId.alphaFirst) {
+        var result = prefix + name + firstId + name + lastId + sufix;
+    } else {
+        var result = prefix + firstId + name + lastId + email + sufix;
+    }
+
+    if (customId.lowerCase) {
+        return result.toLowerCase();
+    } else {
+        return result.toUpperCase();
+    }
 };
--- test.test.js
+++ test.test.js
@@ -1,15 +1,63 @@
 var customId = require("./index");
 
 describe("Is it a string?", () => {
-  let custom = customId({
-    name: "Masud Rana",
-    email: "masudrana@gmail.com",
-    randomLength: 2,
-    lowerCase: true
-  });
+    let custom = customId({
+        name: "Masud Rana",
+        email: "masudrana@gmail.com",
+        randomLength: 2,
+        lowerCase: true
+    });
 
-  const expected = custom;
-  it("Yeah, babe ... It is a fucking string", () => {
-    expect(typeof expected).toBe("string");
-  });
+    const expected = custom;
+    it("Yeah, babe ... It is a string", () => {
+        expect(typeof expected).toBe("string");
+    });
+});
+
+describe("Is it a prefixed id string?", () => {
+    let custom = customId({
+        name: "Numerum Tech",
+        email: "numerum@gmail.com",
+        prefix: 'PRFX'
+    });
+
+    console.log('Generated -->> ', custom);
+
+    const expected = custom;
+    it("Yeah, dude ... It starts with PRF", () => {
+        expect(typeof expected).toBe("string");
+        expect(expected).toMatch(/^PRFX/);
+    });
+});
+
+describe("Is it a sufixed id string?", () => {
+    let custom = customId({
+        name: "Numerum Tech",
+        email: "numerum@gmail.com",
+        sufix: 'SFX'
+    });
+
+    console.log('Generated -->> ', custom);
+
+    const expected = custom;
+    it("Yeah, dude ... It ends with PRF", () => {
+        expect(typeof expected).toBe("string");
+        expect(expected).toMatch(/SFX$/);
+    });
+});
+
+describe("Is it started with alphabetic chars?", () => {
+    let custom = customId({
+        name: "Numerum Tech",
+        email: "numerum@gmail.com",
+        alphaFirst: true
+    });
+
+    console.log('Generated -->> ', custom);
+
+    const expected = custom;
+    it("Yeah, dude ... It starts with alpha chars", () => {
+        expect(typeof expected).toBe("string");
+        expect(expected).toMatch(/^[a-zA-Z]/);
+    });
 });
